87745e118704d3fac31c96e32b6046ba52e8d945
Jorge Huezo <jorgehueso@gmail.com>
2021-04-08 18:14:06 -0500

    Room channel solucion

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -13,7 +13,7 @@ consumer.subscriptions.create("RoomChannel", {
   received(data) {
 
 
-    console.log(data)
+    console.log(data.content)
     // Called when there's incoming data on the websocket for this channel
   }
 });

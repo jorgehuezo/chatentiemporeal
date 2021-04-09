8eda7b76019ff18f4064a9d8db8f605e691ff97a
Jorge Huezo <jorgehueso@gmail.com>
2021-04-08 19:02:13 -0500

    Archivo

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -12,8 +12,8 @@ consumer.subscriptions.create("RoomChannel", {
 
   received(data) {
 
-
-    console.log(data.content)
+$('msg').append('<div class="message"' + data.content + '</div>')
+    console.log(data)
     // Called when there's incoming data on the websocket for this channel
   }
 });

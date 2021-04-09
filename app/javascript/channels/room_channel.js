95fd4a3827fe47885a3c0e0576beade7a7d5b333
Jorge Huezo <jorgehueso@gmail.com>
2021-04-08 19:11:51 -0500

    permanencia del mensaje2

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -12,7 +12,7 @@ consumer.subscriptions.create("RoomChannel", {
 
   received(data) {
 
-$('#messsage_holder').append('<div class="message">' + data.content + '</div>')
+    $('#messsage_holder').append('<div class="message">' + data.content + '</div>')
     console.log(data)
     // Called when there's incoming data on the websocket for this channel
   }

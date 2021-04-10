e67ba69b18cbe818fd3b3f16bc0bb8ec3d3e4251
Jorge Huezo <jorgehueso@gmail.com>
2021-04-09 21:52:14 -0500

    hostingconfiguration static

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
   config.hosts << "a000000000.ngrok.io"   
-
+  config.hosts << "c3e2840591ed.ngrok.io"
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 
